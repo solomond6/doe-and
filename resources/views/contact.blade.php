58b041a524d67191d4d2445f7d01a22709c5f877
Temidayo Uji <solomond6@yahoo.com>
2024-03-28 04:55:43 -0500

    added top icons to the profile component

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -23,21 +23,21 @@
                 <div class="px-6 py-4">
                     <ul class="w-full flex text-center">
                         <li class="w-full flex justify-center">
-                            <a href="#" class="text-gray-600 hover:text-blue-500">
+                            <a href="facebook.com" target="_blank" class="text-gray-600 hover:text-blue-500">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
                                     <path d="M20.875 0H3.125C1.402 0 0 1.402 0 3.125v17.75C0 22.598 1.402 24 3.125 24h9.78v-9.294H9.992V11.25h2.914V9.023c0-2.887 1.765-4.464 4.347-4.464 1.23 0 2.291.092 2.6.134v3.018H17.03c-1.398 0-1.672.665-1.672 1.643V11.25h3.359l-.437 3.456h-2.922V24h5.719C22.598 24 24 22.598 24 20.875V3.125C24 1.402 22.598 0 20.875 0"></path>
                                 </svg>
                             </a>
                         </li>
                         <li class="w-full flex justify-center">
-                            <a href="#" class="text-gray-600 hover:text-blue-500">
+                            <a href="twitter.com" target="_blank" class="text-gray-600 hover:text-blue-500">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
                                   <path d="M24 4.571c-.885.391-1.835.656-2.828.775 1.015-.609 1.8-1.574 2.165-2.723-.951.563-2.005.974-3.129 1.2-.896-.958-2.174-1.559-3.587-1.559-2.72 0-4.923 2.203-4.923 4.923 0 .386.044.758.129 1.117-4.09-.209-7.722-2.193-10.15-5.211-.425.712-.668 1.543-.668 2.423 0 1.692.862 3.185 2.174 4.064-.8-.025-1.553-.246-2.211-.615v.061c0 2.364 1.683 4.328 3.91 4.778-.41.111-.842.171-1.288.171-.315 0-.622-.03-.924-.089.622 1.943 2.429 3.357 4.563 3.394-1.672 1.311-3.779 2.092-6.065 2.092-.393 0-.781-.022-1.165-.067 2.158 1.381 4.722 2.189 7.481 2.189 8.976 0 13.877-7.437 13.877-13.879 0-.211-.005-.422-.015-.631.956-.692 1.793-1.557 2.448-2.542z"></path>
                                 </svg>
                             </a>
                         </li>
                         <li class="w-full flex justify-center">
-                            <a href="#" class="text-gray-600 hover:text-blue-500">
+                            <a href="instagram.com" target="_blank" class="text-gray-600 hover:text-blue-500">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram" fill="currentColor" width="24" height="24">
                                   <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
                                   <path d="M16 11a4 4 0 1 1-8 0 4 4 0 0 1 8 0z"></path>
--- resources/views/livewire/refresh-component.blade.php
+++ resources/views/livewire/refresh-component.blade.php
@@ -1,11 +1,69 @@
 <div wire:click="refreshData">
     <div class="px-6 py-4">
-        <div class="flex items-center" wire:click="refreshData">
-            <img class="h-12 w-12 rounded-full object-cover" src="{{$data['picture']}}" alt="Profile Picture">
-            <div class="ml-4">
-                <h2 class="text-lg fontbold"></h2>
-                <p class="text-gray-600"></p>
+        <div class="flex justify-between">
+            <img class="h-12 w-12 rounded-full object-cover float-left" src="{{$data['picture']}}" alt="Profile Picture">
+            <div class="float-right">
+                <ul class="flex gap-4">
+                    <li>
+                        <a href="#" class="text-gray-600 hover:text-blue-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
+                                <path d="M12 14C15.3137 14 18 11.3137 18 8C18 4.68629 15.3137 2 12 2C8.68629 2 6 4.68629 6 8C6 11.3137 8.68629 14 12 14Z"></path>
+                                <path d="M2 22C2 20.6863 2.84285 19.5211 4.09557 19.1436C5.77848 18.6362 8.4875 18 12 18C15.5125 18 18.2215 18.6362 19.9044 19.1436C21.1571 19.5211 22 20.6863 22 22"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-600 hover:text-blue-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
+                              <path d="M22 6.21l-10 7.69-10-7.69M2 9.3l8 6.13 8-6.13M2 9.3l8 6.13 8-6.13"></path>
+                              <path d="M2 9.3l8 6.13 8-6.13M2 9.3l8 6.13 8-6.13"></path>
+                              <path d="M2 9.3l8 6.13 8-6.13M2 9.3l8 6.13 8-6.13"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-600 hover:text-blue-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone-call">
+                              <path d="M23 1L17.15 5.85C16.291 5.158 15.263 4.831 14.2 4.93L11.54 5.31C9.72 5.57 8.44 6.85 8.17 8.67L7.79 11.33C7.69 12.39 7.36 13.41 6.67 14.27L1 23"></path>
+                              <path d="M16 8.5C16 12.6421 12.6421 16 8.5 16"></path>
+                              <path d="M23 1L17.15 5.85C16.291 5.158 15.263 4.831 14.2 4.93L11.54 5.31C9.72 5.57 8.44 6.85 8.17 8.67L7.79 11.33C7.69 12.39 7.36 13.41 6.67 14.27L1 23"></path>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+                <ul class="flex gap-4">
+                    <li>
+                        <a href="#" class="text-gray-600 hover:text-blue-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone-call">
+                              <path d="M23 1L17.15 5.85C16.291 5.158 15.263 4.831 14.2 4.93L11.54 5.31C9.72 5.57 8.44 6.85 8.17 8.67L7.79 11.33C7.69 12.39 7.36 13.41 6.67 14.27L1 23"></path>
+                              <path d="M16 8.5C16 12.6421 12.6421 16 8.5 16"></path>
+                              <path d="M23 1L17.15 5.85C16.291 5.158 15.263 4.831 14.2 4.93L11.54 5.31C9.72 5.57 8.44 6.85 8.17 8.67L7.79 11.33C7.69 12.39 7.36 13.41 6.67 14.27L1 23"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-600 hover:text-blue-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
+                              <path d="M22 6.21l-10 7.69-10-7.69M2 9.3l8 6.13 8-6.13M2 9.3l8 6.13 8-6.13"></path>
+                              <path d="M2 9.3l8 6.13 8-6.13M2 9.3l8 6.13 8-6.13"></path>
+                              <path d="M2 9.3l8 6.13 8-6.13M2 9.3l8 6.13 8-6.13"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-600 hover:text-blue-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shuffle">
+                                <polyline points="16 3 21 3 21 8"></polyline>
+                                  <line x1="4" y1="20" x2="21" y2="3"></line>
+                                  <polyline points="21 16 21 21 16 21"></polyline>
+                                  <line x1="15" y1="15" x2="21" y2="21"></line>
+                                  <line x1="4" y1="4" x2="9" y2="9"></line>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
             </div>
+            
         </div>
     </div>
      <div class="">
